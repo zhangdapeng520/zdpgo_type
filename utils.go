708d03e0ee0ce7514bad146748bb8769bd47280d
zhangdapeng520 <pygosuperman@outlook.com>
2022-08-29 15:36:21 +0800

    新增：字符串相关方法

--- utils.go
+++ utils.go
@@ -1,6 +1,9 @@
 package zdpgo_type
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strings"
+)
 
 // GetMap 获取map
 func GetMap(keyValues ...interface{}) map[string]interface{} {
@@ -46,4 +49,230 @@ func MapToJson(mapObj map[string]interface{}) (string, error) {
 func JsonToObj(jsonStr string, obj interface{}) error {
 	err := json.Unmarshal([]byte(jsonStr), obj)
 	return err
+}
+
+// SplitArrString 将一维数组按照指定量级拆分为二维数组
+func SplitArrString(arr []string, lines int) [][]string {
+	// 处理数组为nil
+	if arr == nil {
+		return [][]string{}
+	}
+
+	// 行数小于等于0
+	if lines <= 0 {
+		lines = 1
+	}
+
+	// 处理数组长度小于分割行数
+	if len(arr) <= lines {
+		return [][]string{arr}
+	}
+
+	// 切割
+	var result [][]string
+	for i := lines; i <= len(arr); i++ {
+		start := i - lines
+		result = append(result, arr[start:start+lines])
+	}
+	return result
+}
+
+// IsEqualsDoubleArrString 判断两个二维字符串数组是否相等
+func IsEqualsDoubleArrString(arr1, arr2 [][]string) bool {
+	// 两个都为空相等
+	if arr1 == nil && arr2 == nil {
+		return true
+	}
+
+	// 单独一个为空，不相等
+	if arr1 == nil || arr2 == nil {
+		return false
+	}
+
+	// 判断长度
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	// 判断元素
+	for i := 0; i < len(arr1); i++ {
+		arr11 := arr1[i]
+		arr22 := arr2[i]
+
+		// 判断空值
+		if arr11 == nil || arr22 == nil {
+			return false
+		}
+
+		// 判断长度
+		if len(arr11) != len(arr22) {
+			return false
+		}
+
+		// 判断元素
+		for j := 0; j < len(arr11); j++ {
+			if arr11[j] != arr22[j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+// IsEqualsArrString 判断两个字符串数组是否相等
+func IsEqualsArrString(arr1, arr2 []string) bool {
+	// 如果都为空，则相等
+	if arr1 == nil && arr2 == nil {
+		return true
+	}
+
+	// 单独一个为空，不相等
+	if arr1 == nil || arr2 == nil {
+		return false
+	}
+
+	// 判断长度
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	// 判断元素
+	for i := 0; i < len(arr1); i++ {
+		// 判断元素
+		for j := 0; j < len(arr1); j++ {
+			if arr1[j] != arr2[j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+// GetStringSameSlice 获取字符串中相同的连续子串切片
+// @param str1 目标字符串
+// @param str2 要比对的字符串
+// @param slitSep 切割字符串，一般是空格或者换行符
+// @param lines 切割精度，按照多少行进行切割
+// @return [][]int 返回相同的子串的索引切片，可能有多组，每组四个元素，代表[str2开始索引，str2结束索引，str1开始索引，str1结束索引]
+func GetStringSameSlice(str1, str2, splitSep string, lines int) [][]int {
+	arr1 := strings.Split(str1, splitSep)
+	arr2 := strings.Split(str2, splitSep)
+
+	// 拆分数组
+	arr11 := SplitArrString(arr1, lines)
+	arr22 := SplitArrString(arr2, lines)
+
+	// 遍历数组2，查找相同的行
+	var result [][]int
+	for i := 0; i < len(arr22); i++ {
+		for j := 0; j < len(arr11); j++ {
+			if IsEqualsArrString(arr22[i], arr11[j]) {
+				result = append(result, []int{i, i + lines - 1, j, j + lines - 1})
+			}
+		}
+	}
+
+	// 返回
+	return result
+}
+
+// IsEqualsDoubleArrInt 判断两个二维整数数组是否相等
+func IsEqualsDoubleArrInt(arr1, arr2 [][]int) bool {
+	// 两个都为空相等
+	if arr1 == nil && arr2 == nil {
+		return true
+	}
+
+	// 单独一个为空，不相等
+	if arr1 == nil || arr2 == nil {
+		return false
+	}
+
+	// 判断长度
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	// 判断元素
+	for i := 0; i < len(arr1); i++ {
+		arr11 := arr1[i]
+		arr22 := arr2[i]
+
+		// 判断空值
+		if arr11 == nil || arr22 == nil {
+			return false
+		}
+
+		// 判断长度
+		if len(arr11) != len(arr22) {
+			return false
+		}
+
+		// 判断元素
+		for j := 0; j < len(arr11); j++ {
+			if arr11[j] != arr22[j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+// MergeSerialSliceFromDoubleArrInt 从二维整数数组中，合并连续的切片
+// 示例：[][]int{{0, 1, 0, 1}, {1, 2, 1, 2}}
+// 其中第一个元素的0-1和第二个元素的1-2是连续的
+// 合并为：{0, 2, 0, 2}
+func MergeSerialSliceFromDoubleArrInt(arr [][]int) [][]int {
+	var result = make([][]int, 0, 0)
+
+	// 为空
+	if arr == nil {
+		return result
+	}
+
+	// 没有元素
+	if len(arr) == 0 {
+		return result
+	}
+
+	// 不满足元素为4个
+	if len(arr[0]) != 4 {
+		return result
+	}
+
+	// 就1个元素
+	if len(arr) == 1 {
+		result = append(result, arr[0])
+		return result
+	}
+
+	// 开始合并
+	var tempArr = arr[0]
+	for i := 1; i < len(arr); i++ {
+		// 判断是否连续
+		arr1 := arr[i]
+
+		// 不符合规范
+		if arr1[0] > arr1[1] || arr1[2] > arr1[3] {
+			return result
+		}
+
+		// 判断连续
+		if arr1[0] <= tempArr[1] && arr1[2] <= tempArr[3] {
+			tempArr[1] = arr1[1]
+			tempArr[3] = arr1[3]
+		} else {
+			result = append(result, tempArr)
+			tempArr = arr[i]
+		}
+	}
+
+	// 将最后的数组追加
+	result = append(result, tempArr)
+
+	// 返回
+	return result
 }
--- utils_test.go
+++ utils_test.go
@@ -23,3 +23,153 @@ func TestMapToJson(t *testing.T) {
 	}
 	t.Log(obj.A, obj.B)
 }
+
+// 判断两个二维数组是否相等
+func TestIsEqualsDoubleArrString(t *testing.T) {
+	testData := []struct {
+		arg1     [][]string
+		arg2     [][]string
+		expected bool
+	}{
+		{nil, nil, true},
+		{nil, [][]string{{"a"}, {"b"}}, false},
+		{[][]string{{"a"}, {"b"}}, nil, false},
+		{[][]string{{"a"}, {"b"}}, [][]string{{"a"}}, false},
+		{[][]string{{"a"}, {"b"}}, [][]string{{"a"}, {"b"}}, true},
+		{[][]string{{"a"}, {"b"}}, [][]string{{"a", "b"}, {"b"}}, false},
+		{[][]string{{"a"}, {"b"}}, [][]string{{"a", "b"}, {"b", "c"}}, false},
+		{[][]string{{"a", "b"}, {"c", "b"}}, [][]string{{"a", "b"}, {"b", "c"}}, false},
+		{[][]string{{"a", "b"}, {"b", "c"}}, [][]string{{"a", "b"}, {"b", "c"}}, true},
+	}
+
+	for _, tt := range testData {
+		excepted := IsEqualsDoubleArrString(tt.arg1, tt.arg2)
+		if excepted != tt.expected {
+			t.Errorf("%v %v : expected %v, got %v", tt.arg1, tt.arg2, tt.expected, excepted)
+		}
+	}
+}
+
+// 判断两个字符串数组是否相等
+func TestIsEqualsArrString(t *testing.T) {
+	testData := []struct {
+		arg1     []string
+		arg2     []string
+		expected bool
+	}{
+		{nil, nil, true},
+		{nil, []string{"a", "b"}, false},
+		{[]string{"a", "b"}, nil, false},
+		{[]string{"a", "b"}, []string{"a"}, false},
+		{[]string{"a", "b"}, []string{"a", "b"}, true},
+		{[]string{"a", "b"}, []string{"a", "b", "b"}, false},
+		{[]string{"a", "b"}, []string{"a", "b", "b", "c"}, false},
+		{[]string{"a", "b", "c", "b"}, []string{"a", "b", "b", "c"}, false},
+		{[]string{"a", "b", "b", "c"}, []string{"a", "b", "b", "c"}, true},
+	}
+
+	for _, tt := range testData {
+		excepted := IsEqualsArrString(tt.arg1, tt.arg2)
+		if excepted != tt.expected {
+			t.Errorf("expected %v, got %v", excepted, tt.expected)
+		}
+	}
+}
+
+// 测试将一维数组拆分为二维数组
+func TestSplitArrString(t *testing.T) {
+	testData := []struct {
+		arg      []string
+		lines    int
+		expected [][]string
+	}{
+		{[]string{"a", "b", "c"}, 2, [][]string{{"a", "b"}, {"b", "c"}}},
+		{[]string{"a", "b"}, 2, [][]string{{"a", "b"}}},
+		{[]string{"a"}, 2, [][]string{{"a"}}},
+		{nil, 2, [][]string{}},
+		{[]string{"a"}, 0, [][]string{{"a"}}},
+		{[]string{"a", "b"}, 0, [][]string{{"a"}, {"b"}}},
+	}
+
+	for _, tt := range testData {
+		result := SplitArrString(tt.arg, tt.lines)
+		if !IsEqualsDoubleArrString(result, tt.expected) {
+			t.Errorf("expected %v but got %v", tt.expected, result)
+		}
+	}
+}
+
+// 测试字符串中相同的连续子串
+func TestGetStringSameSlice(t *testing.T) {
+	testData := []struct {
+		str1     string
+		str2     string
+		splitSep string
+		lines    int
+		expected [][]int
+	}{
+		{"a b c", "a b c d e", " ", 2,
+			[][]int{
+				{0, 1, 0, 1},
+				{1, 2, 1, 2},
+			},
+		},
+		{"a b c d e f g", "a b c d e h i j k l m n", " ", 4,
+			[][]int{
+				{0, 3, 0, 3},
+				{1, 4, 1, 4},
+			},
+		},
+	}
+
+	for _, tt := range testData {
+		result := GetStringSameSlice(tt.str1, tt.str2, tt.splitSep, tt.lines)
+		if !IsEqualsDoubleArrInt(result, tt.expected) {
+			t.Errorf("expected %v, got %v", tt.expected, result)
+		}
+	}
+}
+
+// 测试二维整数数组合并
+func TestMergeSerialSliceFromDoubleArrInt(t *testing.T) {
+	testData := []struct {
+		arr      [][]int
+		expected [][]int
+	}{
+		{
+			nil,
+			[][]int{},
+		},
+		{
+			[][]int{},
+			[][]int{},
+		},
+		{
+			[][]int{{0, 1, 0, 1}},
+			[][]int{{0, 1, 0, 1}},
+		},
+		{
+			[][]int{{0, 1, 0, 1}, {1, 2, 1, 2}},
+			[][]int{{0, 2, 0, 2}},
+		},
+		{
+			[][]int{{0, 3, 0, 3}, {1, 4, 1, 4}},
+			[][]int{{0, 4, 0, 4}},
+		},
+		{
+			[][]int{{0, 3, 0, 3}, {1, 4, 1, 4}, {5, 8, 10, 13}},
+			[][]int{{0, 4, 0, 4}, {5, 8, 10, 13}},
+		},
+		{
+			[][]int{{0, 3, 0, 3}, {1, 4, 1, 4}, {5, 8, 10, 13}, {7, 10, 12, 15}},
+			[][]int{{0, 4, 0, 4}, {5, 10, 10, 15}},
+		},
+	}
+
+	for _, tt := range testData {
+		result := MergeSerialSliceFromDoubleArrInt(tt.arr)
+		if !IsEqualsDoubleArrInt(result, tt.expected) {
+			t.Errorf("expected %v, got %v", tt.expected, result)
+		}
+	}
+}
